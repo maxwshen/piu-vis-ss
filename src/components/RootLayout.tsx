cd00a411996596d4efcf96acf297d6a07ebd3822
Max Shen <maxwshen@gmail.com>
2024-12-21 14:50:17 -0500

    add gtag

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,7 +1,7 @@
 // src/components/RootLayout.tsx
 import { ParentComponent, onMount, onCleanup } from "solid-js";
 import { useNavigate } from "@solidjs/router";
-
+import { Meta } from "@solidjs/meta";
 
 const RootLayout: ParentComponent = (props) => {
   const navigate = useNavigate();
@@ -42,6 +42,21 @@ const RootLayout: ParentComponent = (props) => {
 
   onMount(() => {
     document.addEventListener('click', handleLinkClick);
+
+    // Add Google Analytics
+    const script1 = document.createElement('script');
+    script1.async = true;
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-2XBRXQWGP5";
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-2XBRXQWGP5');
+    `;
+    document.head.appendChild(script2);
   });
 
   onCleanup(() => {
